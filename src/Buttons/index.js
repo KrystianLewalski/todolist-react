713b744c2121b4d508927379691fe931c55acd2d
Krystian Lewalski <Krystian.Lewalski@interia.pl>
2023-06-08 18:40:20 +0200

    toggleHideDone button fix

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -5,7 +5,7 @@ const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
         {tasks.length > 0 && (
             <>
                 <button onClick={toggleHideDone} className="buttons">
-                    {hideDone ? "Ukryj" : "Pokaż"} ukończone
+                    {hideDone ? "Pokaż" : "Ukryj"} ukończone
                 </button>
                 <button
                     onClick={setAllDone}
